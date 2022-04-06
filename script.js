24797c58d87e56011ed1e64e8bb3760bc310a2f3
LuanSevero <luansevero@icloud.com>
2022-04-06 14:23:22 -0300

    Função adicionar src para a cardBackFace e sortea a mesma

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function isANumber(){
+function startGame(){
+    const arrayImgsdataBase = ["backCards/bobrossparrot.gif", "backCards/bobrossparrot.gif","backCards/explodyparrot.gif","backCards/explodyparrot.gif","backCards/fiestaparrot.gif","backCards/fiestaparrot.gif","backCards/metalparrot.gif","backCards/metalparrot.gif","backCards/revertitparrot.gif","backCards/revertitparrot.gif","backCards/tripletsparrot.gif","backCards/tripletsparrot.gif","backCards/unicorn.gif","backCards/unicorn.gif",]
+    let arrayImgs = []
     let number = prompt("Quantas cartas você quer jogar?")
         number = Number(number)
     for(let i = 0; i == i; i++){
@@ -7,8 +9,22 @@ function isANumber(){
         }
         number = prompt("Quantas cartas você quer jogar?")
     }
-    const cartas = document.querySelectorAll(`.card`);
+    //Final do Verificador de Número
+    //Embaralhador de Cartas
     for(let i = 0; i < number; i++){
-        cartas[i].classList.add(`show`)
+        arrayImgs.push(arrayImgsdataBase[i])
+    }
+    arrayImgs.sort(comparador)
+    function comparador() { 
+	return Math.random() - 0.5; 
+    }
+    const cartas = document.querySelectorAll(`.card`); //Saber quantas cartas vão estar em jogo.
+    const imgBackFace = document.querySelectorAll(`.imgBackFace`) //Pegar o src das cartas para poder mudar
+    for(let i = 0; i < number; i++){ //Limitador = número de cartas digitada pelo usuário
+        cartas[i].classList.add(`show`) //Dar classe para as cartas que vão aparecer.
+        imgBackFace[i].src = arrayImgs[i] //Mudar src das image(Já sorteados)
     }
 }
+
+
+ 
